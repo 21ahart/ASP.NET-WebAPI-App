cae0120215cc5b9143b9053fd74749c8dc8be8e2
SebastianRZapata <zapatasn@mail.uc.edu>
2025-11-15 20:47:07 -0500

    Added model, controllers folders, student get functionality (http GET endpoint)

---
+++ MyWebApi/Controllers/StudentsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyWebApi.Data;
+using MyWebApi.Models;
+
+namespace MyWebApi.Controllers
+{
+    [ApiController]
+    [Route ("api/[controller]")] //route is api/students for now
+    public class StudentsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public StudentsController(ApplicationDbContext db) => _db = db;
+
+        // GET
+        [HttpGet("{id?}")] // ? = can be null
+        public async Task<IActionResult> GetStudents(int? id)
+        {
+            if(!id.HasValue ||  id.Value == 0) //if id is missing or 0, return first 5
+            {
+                var firstFive = await _db.Students
+                    .OrderBy(s => s.Id)
+                    .Take(5)
+                    .ToListAsync();
+
+                return Ok(firstFive);
+            }
+
+            var student = await _db.Students.FindAsync(id.Value);
+            if (student == null) //find by key, if not found = 404, else 200 w/ student data
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+    }
+}
---
+++ MyWebApi/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MyWebApi.Models
+{
+    //Force one of these options (stored as int 0-4 - Change if str preferred).
+    public enum YearInProgram
+    {
+        Freshman,
+        Sophomore,
+        Junior,
+        Senior,
+        Graduate
+    }
+
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }  //primary key
+
+        [Required]
+        [MaxLength(100)]
+        public string StudentName { get; set; } = string.Empty;
+
+        public DateTime Birthdate { get; set; } // birthdate
+
+        [MaxLength(100)]
+        public string CollegeProgram { get; set; } = string.Empty; //Student college program
+
+        public YearInProgram YearInProgram { get; set; } = YearInProgram.Freshman; //set freshman as default
+
+
+    }
+}
